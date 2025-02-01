fd8ac8035b5736a93a604a215f2cbd377144582e
Elias Grinwis Plaat Stultjes <egrinwisplaatstultjes@cubigo.com>
2025-02-01 20:53:20 +0100

    Refactor project

---
+++ backend/Luxora.Infrastructure/Context/LuxoraDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Luxora.Domain.Entities.EFService;
+using Luxora.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luxora.Infrastructure.Context;
+
+public class LuxoraDbContext : DbContext
+{
+    public LuxoraDbContext(DbContextOptions<LuxoraDbContext> options) : base(options) { }
+    
+    public DbSet<Product> Products { get; set; }
+    
+    public DbSet<Service> Services { get; set; }
+    
+    public DbSet<ServiceCategory> ServiceCategories { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().ToTable("Products");
+        modelBuilder.Entity<Service>().ToTable("Services");
+        modelBuilder.Entity<ServiceCategory>().ToTable("ServiceCategories");
+    }
+}
