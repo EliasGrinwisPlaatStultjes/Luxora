7d559099174ec3de6932d155ef493338a8b444e4
Elias Grinwis Plaat Stultjes <egrinwisplaatstultjes@cubigo.com>
2025-02-03 19:45:35 +0100

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly. some extra text that i only want to see in dev</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
